a0b786f68501c4d89aab0489af2fab7cbbaf272c
sidali zitouni terki <sidalizitouniterki@MacBook-Air-de-sidali.local>
2023-12-01 21:17:38 +0100

    correction carre_siamois_so et carre_siamois_se

--- carre_siamois.py
+++ carre_siamois.py
@@ -46,8 +46,8 @@ def carre_siamois_se(n : int,k : int) -> List [int]:
     carre = [[0 for i in range(n)] for j in range(n)]
     # On initialise l qui est le nombre de chiffres rajoutés au carré
     l = 1
-    # On initialise i qui sera le numero de ligne à 0
-    i = 0
+    # On initialise i qui sera le numero de la derniere ligne
+    i =  n- 1
     # On initialise j qui sera le numero de colonne qui sera le milieu de la ligne
     j =  int((n-1)/2)
     # valeur de départ que donnera la personne qui utilise la fonction
@@ -59,8 +59,8 @@ def carre_siamois_se(n : int,k : int) -> List [int]:
         # On verifie si le case Sud Est de la precedente case
         # est rempli
         if (carre[(i + 1) % n][(j + 1) % n] != 0):
-            # alors on se deplace d'une case à gauche
-            j = (j - 1) % n
+            # alors on monte d'une case
+            i = (i - 1) % n
             # on augmente la valeur precedente
             k += 1
             carre[i][j] = k
@@ -123,8 +123,8 @@ def carre_siamois_so(n : int,k : int) -> List [int]:
     carre = [[0 for i in range(n)] for j in range(n)]
     # On initialise l qui est le nombre de chiffres rajoutés au carré
     l = 1
-    # On initialise i qui sera le numero de ligne à 0
-    i = 0
+    # On initialise i qui sera le numero de la derniere ligne
+    i =  n- 1
     # On initialise j qui sera le numero de colonne qui sera le milieu de la ligne
     j =  int((n-1)/2)
     # valeur de départ que donnera la personne qui utilise la fonction
@@ -136,8 +136,8 @@ def carre_siamois_so(n : int,k : int) -> List [int]:
         # On verifie si le case Sud Ouest de la precedente case
         # est rempli
         if (carre[(i + 1) % n][(j - 1) % n] != 0):
-            # alors on se deplace d'une case à gauche
-            j = (j + 1) % n
+            # alors on monte d'une case
+            i = (i - 1) % n
             # on augmente la valeur precedente
             k += 1
             carre[i][j] = k
@@ -154,4 +154,3 @@ def carre_siamois_so(n : int,k : int) -> List [int]:
         # On rajoute + 1 aux nombres de chiffres rajoutés
         l += 1
     return carre
-
