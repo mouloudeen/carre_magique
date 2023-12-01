0e1157775c37ad1dfece11e4a77d71950fb0ca29
sidali zitouni terki <sidalizitouniterki@MacBook-Air-de-sidali.local>
2023-12-01 19:27:15 +0100

    ajout carré de façcon Sud Ouest, Nord Ouest et Sud Est et leurs testes

--- carre_siamois.py
+++ carre_siamois.py
@@ -1,19 +1,157 @@
 from typing import List
+
+#Siamois Nord Est
 def carre_siamois_ne(n : int,k : int) -> List [int]:
+    # Creation d'un carré rempli de 0
     carre = [[0 for i in range(n)] for j in range(n)]
+    # On initialise l qui est le nombre de chiffres rajoutés au carré
     l = 1
-    i = 0 
+    # On initialise i qui sera le numero de ligne à 0
+    i = 0
+    # On initialise j qui sera le numero de colonne qui sera le milieu de la ligne
     j =  int((n-1)/2)
+    # valeur de départ que donnera la personne qui utilise la fonction
     carre[i][j] = k
+    
+    #Tant que le carré n'est pas rempli on continue
     while l != n*n:
+    
+        # On verifie si le case Nord-Est de la precedente case
+        # est rempli
         if (carre[(i - 1) % n][(j + 1) % n] != 0):
+            # alors on descend d'une case
             i = (i + 1) % n
+            # on augmente la valeur precedente
             k += 1
             carre[i][j] = k
+            # On rajoute + 1 aux nombres de chiffres rajoutés
             l += 1
+            
+        # Comme la case Nord-Est de la precedente case n'est pas rempli
         i = (i - 1) % n
         j = (j + 1) % n
+        # on augmente la valeur precedente
         k += 1
+        # On rempli cette case Nord-Est  avec la valeur augmente
         carre[i][j] = k
+        # On rajoute + 1 aux nombres de chiffres rajoutés
         l += 1
     return carre
+    
+    
+
+#Siamois Sud Est
+def carre_siamois_se(n : int,k : int) -> List [int]:
+    # Creation d'un carré rempli de 0
+    carre = [[0 for i in range(n)] for j in range(n)]
+    # On initialise l qui est le nombre de chiffres rajoutés au carré
+    l = 1
+    # On initialise i qui sera le numero de ligne à 0
+    i = 0
+    # On initialise j qui sera le numero de colonne qui sera le milieu de la ligne
+    j =  int((n-1)/2)
+    # valeur de départ que donnera la personne qui utilise la fonction
+    carre[i][j] = k
+    
+    #Tant que le carré n'est pas rempli on continue
+    while l != n*n:
+    
+        # On verifie si le case Sud Est de la precedente case
+        # est rempli
+        if (carre[(i + 1) % n][(j + 1) % n] != 0):
+            # alors on se deplace d'une case à gauche
+            j = (j - 1) % n
+            # on augmente la valeur precedente
+            k += 1
+            carre[i][j] = k
+            # On rajoute + 1 aux nombres de chiffres rajoutés
+            l += 1
+            
+        # Comme la case Sud Est de la precedente case n'est pas rempli
+        i = (i + 1) % n
+        j = (j + 1) % n
+        # on augmente la valeur precedente
+        k += 1
+        # On rempli cette case Sud Est  avec la valeur augmente
+        carre[i][j] = k
+        # On rajoute + 1 aux nombres de chiffres rajoutés
+        l += 1
+    return carre
+    
+
+#Siamois Nord Ouest
+def carre_siamois_no(n : int,k : int) -> List [int]:
+    # Creation d'un carré rempli de 0
+    carre = [[0 for i in range(n)] for j in range(n)]
+    # On initialise l qui est le nombre de chiffres rajoutés au carré
+    l = 1
+    # On initialise i qui sera le numero de ligne à 0
+    i = 0
+    # On initialise j qui sera le numero de colonne qui sera le milieu de la ligne
+    j =  int((n-1)/2)
+    # valeur de départ que donnera la personne qui utilise la fonction
+    carre[i][j] = k
+    
+    #Tant que le carré n'est pas rempli on continue
+    while l != n*n:
+    
+        # On verifie si le case Nord Ouest de la precedente case
+        # est rempli
+        if (carre[(i - 1) % n][(j - 1) % n] != 0):
+            # alors on descend d'une case
+            i = (i + 1) % n
+            # on augmente la valeur precedente
+            k += 1
+            carre[i][j] = k
+            # On rajoute + 1 aux nombres de chiffres rajoutés
+            l += 1
+            
+        # Comme la case Nord Ouest de la precedente case n'est pas rempli
+        i = (i - 1) % n
+        j = (j - 1) % n
+        # on augmente la valeur precedente
+        k += 1
+        # On rempli cette case Nord Ouest  avec la valeur augmente
+        carre[i][j] = k
+        # On rajoute + 1 aux nombres de chiffres rajoutés
+        l += 1
+    return carre
+    
+#Siamois Sud Ouest
+def carre_siamois_so(n : int,k : int) -> List [int]:
+    # Creation d'un carré rempli de 0
+    carre = [[0 for i in range(n)] for j in range(n)]
+    # On initialise l qui est le nombre de chiffres rajoutés au carré
+    l = 1
+    # On initialise i qui sera le numero de ligne à 0
+    i = 0
+    # On initialise j qui sera le numero de colonne qui sera le milieu de la ligne
+    j =  int((n-1)/2)
+    # valeur de départ que donnera la personne qui utilise la fonction
+    carre[i][j] = k
+    
+    #Tant que le carré n'est pas rempli on continue
+    while l != n*n:
+    
+        # On verifie si le case Sud Ouest de la precedente case
+        # est rempli
+        if (carre[(i + 1) % n][(j - 1) % n] != 0):
+            # alors on se deplace d'une case à gauche
+            j = (j + 1) % n
+            # on augmente la valeur precedente
+            k += 1
+            carre[i][j] = k
+            # On rajoute + 1 aux nombres de chiffres rajoutés
+            l += 1
+            
+        # Comme la case Sud Ouest de la precedente case n'est pas rempli
+        i = (i + 1) % n
+        j = (j - 1) % n
+        # on augmente la valeur precedente
+        k += 1
+        # On rempli cette case Sud Ouest  avec la valeur augmente
+        carre[i][j] = k
+        # On rajoute + 1 aux nombres de chiffres rajoutés
+        l += 1
+    return carre
+
--- test_carre_magique.py
+++ test_carre_magique.py
@@ -5,12 +5,33 @@ import outil_carre_magique
 n = 7
 k = 4
 
-carre = carre_siamois.carre_siamois_ne(n,k)
-print(f'carre siamois d ordre {n} avec comme début {k}')
+carre_ne = carre_siamois.carre_siamois_ne(n,k)
+print(f'carre siamois Nord Est d ordre {n} avec comme début {k}')
 for i in range(n):
-    print (f'{carre[i]} ')
+    print (f'{carre_ne[i]} ')
 
-print(f'Est ce un carré magique : {outil_carre_magique.carre_is_magic(carre,n)} ')
+print(f'Est ce un carré magique : {outil_carre_magique.carre_is_magic(carre_ne,n)} ')
+
+carre_se = carre_siamois.carre_siamois_se(n,k)
+print(f'carre siamois Sud Est d ordre {n} avec comme début {k}')
+for i in range(n):
+    print (f'{carre_se[i]} ')
+
+print(f'Est ce un carré magique : {outil_carre_magique.carre_is_magic(carre_se,n)} ')
+
+carre_no = carre_siamois.carre_siamois_no(n,k)
+print(f'carre siamois Nord Ouest d ordre {n} avec comme début {k}')
+for i in range(n):
+    print (f'{carre_no[i]} ')
+
+print(f'Est ce un carré magique : {outil_carre_magique.carre_is_magic(carre_no,n)} ')
+
+carre_so = carre_siamois.carre_siamois_so(n,k)
+print(f'carre siamois Sud Ouest d ordre {n} avec comme début {k}')
+for i in range(n):
+    print (f'{carre_so[i]} ')
+
+print(f'Est ce un carré magique : {outil_carre_magique.carre_is_magic(carre_so,n)} ')
 
 print("on teste avec un ordre impaire  alétoire et le début aléatoire")
 
@@ -19,6 +40,14 @@ while (n%2 != 1):
 	n = random.randint(1,10000)
 k = random.randint(1,10000)
 
-carre = carre_siamois.carre_siamois_ne(n,k)
+
 print(f'carre siamois d ordre {n} avec comme début {k}')
-print(f'Est ce un carré magique : {outil_carre_magique.carre_is_magic(carre,n)} ')
+
+carre_ne = carre_siamois.carre_siamois_ne(n,k)
+carre_no = carre_siamois.carre_siamois_no(n,k)
+carre_se = carre_siamois.carre_siamois_se(n,k)
+carre_so = carre_siamois.carre_siamois_so(n,k)
+print(f'Est ce un carré façons Nord-Est est magique : {outil_carre_magique.carre_is_magic(carre_ne,n)} ')
+print(f'Est ce un carré façons Nord-Ouest est magique : {outil_carre_magique.carre_is_magic(carre_no,n)} ')
+print(f'Est ce un carré façons Sud-Est est magique : {outil_carre_magique.carre_is_magic(carre_se,n)} ')
+print(f'Est ce un carré façons Sud-Ouest est magique : {outil_carre_magique.carre_is_magic(carre_so,n)} ')
