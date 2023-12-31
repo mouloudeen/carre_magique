87c6224e403ee35245bc93ae8cfd72b3351bbfc4
sidali zitouni terki <sidalizitouniterki@MacBook-Air-de-sidali.local>
2023-12-31 20:07:54 +0100

    implementation en pour le carre magique lozenge avec quelques tests

--- carre_lozenge/C/carre_lozenge/carre_lozenge/carre_lozenge.h
+++ carre_lozenge/C/carre_lozenge/carre_lozenge/carre_lozenge.h
@@ -9,5 +9,25 @@
 #define carre_lozenge_h
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*On commence à remplir les nombres impairs en forme de losange*/
+int ** remplir_impair(int , int);
+
+/*On remplit le coté droit en nombre pair qui sont les (ordre-1)^2 premiers nombres pairs*/
+int ** remplir_pair_droite(int **, int , int);
+
+/*On remplit le coté gauche en nombre pair qui sont entre (ordre-1)^2 et ordre^2-1*/
+int ** remplir_pair_gauche(int ** , int);
+
+/* carre magique en forme de lozenge*/
+int ** carre_lozenge(int);
+
+/* Affichage du carré magique*/
+void AffichageCarre(int ** , int);
+
+/* Desalloué la mémoire du carre */
+void Free_carre(int **,int);
 
 #endif /* carre_lozenge_h */
--- carre_lozenge/C/carre_lozenge/carre_lozenge/main.c
+++ carre_lozenge/C/carre_lozenge/carre_lozenge/main.c
@@ -5,10 +5,89 @@
 //  Created by sidali zitouni terki on 31/12/2023.
 //
 
-#include <stdio.h>
+
+#include "carre_lozenge.h"
+#include "outil_carre.h"
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int ordre = 5;
+    
+    int ** carre_5 = carre_lozenge(ordre);
+    
+    /* On teste les premiers nombres impairs */
+    printf("carre d'ordre %d \n",ordre);
+    AffichageCarre(carre_5,ordre);
+    
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre_5, ordre));
+    
+  
+    ordre = 7;
+    
+    int ** carre_7 = carre_lozenge(ordre);
+    
+    printf("carre d'ordre %d \n",ordre);
+    AffichageCarre(carre_7,ordre);
+    
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre_7, ordre));
+    
+    ordre = 9;
+    
+    int ** carre_9 = carre_lozenge(ordre);
+    
+    printf("carre d'ordre %d \n",ordre);
+    AffichageCarre(carre_9,ordre);
+    
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre_9, ordre));
+    
+    ordre = 11;
+    
+    int ** carre_11 = carre_lozenge(ordre);
+    
+    printf("carre d'ordre %d \n",ordre);
+    AffichageCarre(carre_11,ordre);
+    
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre_11, ordre));
+    
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre_11, ordre));
+    
+    ordre = 13;
+    
+    int ** carre_13 = carre_lozenge(ordre);
+    
+    printf("carre d'ordre %d \n",ordre);
+    AffichageCarre(carre_13,ordre);
+    
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre_13, ordre));
+    
+/*on teste avec des grands ordres impairs pris au random*/
+    
+    ordre = 5301;
+    int ** carre1 = carre_lozenge(ordre);
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre1, ordre));
+    
+    ordre = 7339;
+    carre1 = carre_lozenge(ordre);
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre1, ordre));
+    
+    ordre = 9591;
+    carre1 = carre_lozenge(ordre);
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre1, ordre));
+    
+    ordre = 3687;
+    carre1 = carre_lozenge(ordre);
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre1, ordre));
+    
+    ordre = 7243;
+    carre1 = carre_lozenge(ordre);
+    printf("carre d'ordre %d est magique: %d \n",ordre,carre_est_magique(carre1, ordre));
+    
+    Free_carre(carre_5,5);
+    Free_carre(carre_7,7);
+    Free_carre(carre_9,9);
+    Free_carre(carre_11,11);
+    Free_carre(carre_13,13);
+    Free_carre(carre1,7243);
     return 0;
 }
