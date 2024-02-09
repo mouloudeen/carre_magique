defc15f68cf57238311622d55bec35b14eb55903
sidali zitouni terki <sidalizitouniterki@MacBook-Air-de-sidali.local>
2024-02-09 14:56:06 +0100

    changemnt des fonctions dans permutation_diagonale avec ajout d'un début et de nombre pas

--- paire_paire/permutation_diagonale/python/permutation_diagonale.py
+++ paire_paire/permutation_diagonale/python/permutation_diagonale.py
@@ -1,23 +1,23 @@
 from typing import List
 
 # On remplit partant du coordonnée (i,j) avec comme déplacement
-# (idep,jdep)
-def initialisation(carre : List[int],i: int,j : int, idep: int, jdep: int,n : int) :
+# (idep,jdep) avec pas qui est le nombre de pas et k le debut
+def initialisation(carre : List[int],i: int,j : int, idep: int, jdep: int, pas: int, k: int, n : int) :
     cpt = 1
     # on remplit la 1er valeur de coordonnée (i,j)
-    carre[i][j]= i*n + j+1
+    carre[i][j]= (i*n + j+k)*pas
     # puis on continue en se deplaçant (n-1) fois
     while cpt < n :
         i = (i +idep)%n
         j = (j+jdep)%n
-        carre[i][j]= i*n + j+1
+        carre[i][j]= (i*n + j+k)*pas
         cpt += 1
     
 
 
-def permu_diag(n : int) -> List[int]:
+def permu_diag(n : int, pas : int, k : int) -> List[int]:
     # genere un carre d'ordre n avec les indices inversé et en ajoutant 1
-    carre = [[(n*n+1)- (i*n + j+1) for j in range(n) ] for i in range(n)]
+    carre = [[pas*((n*n+1)- (i*n + j+1)+k-1) for j in range(n) ] for i in range(n)]
     
     # cpt est égale au quotient de n/4 (on sait que n est divisible par 4)
     cpt = int(n/4)
@@ -27,7 +27,10 @@ def permu_diag(n : int) -> List[int]:
     j1 = n-1
     # on le fait autant que la valeur cpt
     for i in range(cpt):
-        initialisation(carre,i0,j0, 1, 1,n)
-        initialisation(carre,i0,j1, 1, -1,n)
+        initialisation(carre,i0,j0, 1, 1,pas,k,n)
+        initialisation(carre,i0,j1, 1, -1,pas,k,n)
         i0 += 4
     return carre
+carre = permu_diag(4,2,2)
+for i in range(4):
+    print(f'{carre[i]}')
--- paire_paire/permutation_diagonale/python/test_carre_magique.py
+++ paire_paire/permutation_diagonale/python/test_carre_magique.py
@@ -4,7 +4,7 @@ import outil_carre_magique
 
 # On teste les premiers nombres divisible par 4
 n = 4
-carre_4 = permutation_diagonale.permu_diag(n)
+carre_4 = permutation_diagonale.permu_diag(n,1,1)
 
 for i in range(n):
     print(f'{carre_4[i]}')
@@ -13,7 +13,7 @@ print(f' carre d ordre {n} est magique {outil_carre_magique.carre_is_magic(carre
 
 
 n = 8
-carre_8 = permutation_diagonale.permu_diag(n)
+carre_8 = permutation_diagonale.permu_diag(n,1,1)
 
 for i in range(n):
     print(f'{carre_8[i]}')
@@ -21,7 +21,7 @@ for i in range(n):
 print(f' carre d ordre {n} est magique {outil_carre_magique.carre_is_magic(carre_8,n)}\n')
 
 n = 12
-carre_12 = permutation_diagonale.permu_diag(n)
+carre_12 = permutation_diagonale.permu_diag(n,1,1)
 
 for i in range(n):
     print(f'{carre_12[i]}')
@@ -29,7 +29,7 @@ for i in range(n):
 print(f' carre d ordre {n} est magique {outil_carre_magique.carre_is_magic(carre_12,n)}\n')
 
 n = 16
-carre_16 = permutation_diagonale.permu_diag(n)
+carre_16 = permutation_diagonale.permu_diag(n,1,1)
 
 for i in range(n):
     print(f'{carre_16[i]}')
@@ -41,21 +41,21 @@ print(f' carre d ordre {n} est magique {outil_carre_magique.carre_is_magic(carre
 #on teste avec des grands ordres impairs pris au random
 
 n = 2380
-carre_1 = permutation_diagonale.permu_diag(n)
+carre_1 = permutation_diagonale.permu_diag(n,1,1)
 print(f' carre d ordre {n} est magique {outil_carre_magique.carre_is_magic(carre_1,n)}\n')
 
 n = 4472
-carre_1 = permutation_diagonale.permu_diag(n)
+carre_1 = permutation_diagonale.permu_diag(n,1,1)
 print(f' carre d ordre {n} est magique {outil_carre_magique.carre_is_magic(carre_1,n)}\n')
 
 n = 8312
-carre_1 = permutation_diagonale.permu_diag(n)
+carre_1 = permutation_diagonale.permu_diag(n,1,1)
 print(f' carre d ordre {n} est magique {outil_carre_magique.carre_is_magic(carre_1,n)}\n')
 
 n = 6244
-carre_1 = permutation_diagonale.permu_diag(n)
+carre_1 = permutation_diagonale.permu_diag(n,1,1)
 print(f' carre d ordre {n} est magique {outil_carre_magique.carre_is_magic(carre_1,n)}\n')
 
 n = 6088
-carre_1 = permutation_diagonale.permu_diag(n)
+carre_1 = permutation_diagonale.permu_diag(n,1,1)
 print(f' carre d ordre {n} est magique {outil_carre_magique.carre_is_magic(carre_1,n)}\n')
